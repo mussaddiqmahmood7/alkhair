d8a656ab5013e9409332c686b3d82a5b83dce1fb
mussaddiqmahmood7 <mussaddiqmahmood7@gmail.com>
2025-11-28 22:02:58 +0500

    update the blob env

--- lib/uploadImage.ts
+++ lib/uploadImage.ts
@@ -4,8 +4,10 @@ import { compressImage } from "./compressImage";
 export async function uploadImage(file: File) {
   const compressedFile = await compressImage(file);
 
+  const BLOB_TOKEN = process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN; 
   const blob = await put(`products/${Date.now()}-${file.name}`, compressedFile, {
     access: "public",
+    token:BLOB_TOKEN
   });
 
   return blob.url; // final image URL
